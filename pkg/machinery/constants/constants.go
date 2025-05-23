c11c7b33895eb163f7b51e69d109ac1f4a2e3b3e
Jean-Francois Roy <jf@devklog.net>
2025-05-23 15:15:47 -0700

    chore: update default kernel version

--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	// DefaultKernelVersion is the default Linux kernel version.
-	DefaultKernelVersion = "6.12.57-talos"
+	DefaultKernelVersion = "6.16.12-talos"
 
 	// KernelParamConfig is the kernel parameter name for specifying the URL.
 	// to the config.
